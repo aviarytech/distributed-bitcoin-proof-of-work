46adf0ae2e70d4438cfc66d76b6221472bb9aa7d
Brian Richter <brianorwhatever@gmail.com>
2020-09-07 00:02:20 -0700

    dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM elixir:1.10.4
+
+RUN mkdir /code
+ADD *
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pro1.Mixfile do
     [
       app: :project1,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10.4",
       start_permanent: Mix.env == :prod,
       escript: [main_module: Project1],
       deps: deps()

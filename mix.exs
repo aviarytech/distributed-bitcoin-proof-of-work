13ee6ac85459ad501ffa0f1258dd8206ecea723f
Brian Richter <brianorwhatever@gmail.com>
2020-09-07 17:06:05 -0700

    updated elixir version and bc

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pro1.Mixfile do
     [
       app: :project1,
       version: "0.1.0",
-      elixir: "~> 1.10.4",
+      elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       escript: [main_module: Project1],
       deps: deps()
